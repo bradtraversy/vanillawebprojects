8389767b8b839bcb840b278f5286a545974e1924
Brad Traversy <traversymedia@gmail.com>
2020-01-28 09:23:26 -0500

    Added new-year-countdown project

---
+++ new-year-countdown/README.md
@@ -0,0 +1,11 @@
+## New Year Countdown
+
+Landing page that counts down from the current date to the next new year
+
+## Project Specifications
+
+- Create landing page with HTML/CSS
+- Calculate the days, hours, mins and seconds to the new year
+- Insert values into the DOM
+- Show a spinner right before loading the countdown
+- Show the coming year in the background
---
+++ new-year-countdown/script.js
@@ -0,0 +1,40 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countdown time
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  // Add values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
+
+// Run every second
+setInterval(updateCountdown, 1000);
