351f98ba1ddbc63bc3226cec76a3184d1c9aaa86
Brad Traversy <traversymedia@gmail.com>
2020-01-29 08:02:11 -0500

    Fixed case issue

--- hangman/script.js
+++ hangman/script.js
@@ -16,101 +16,101 @@ const wrongLetters = [];
 
 // Show hidden word
 function displayWord() {
-  wordEl.innerHTML = `
+	wordEl.innerHTML = `
     ${selectedWord
-      .split('')
-      .map(
-        letter => `
+			.split('')
+			.map(
+				letter => `
           <span class="letter">
             ${correctLetters.includes(letter) ? letter : ''}
           </span>
         `
-      )
-      .join('')}
+			)
+			.join('')}
   `;
 
-  const innerWord = wordEl.innerText.replace(/\n/g, '');
+	const innerWord = wordEl.innerText.replace(/\n/g, '');
 
-  if (innerWord === selectedWord) {
-    finalMessage.innerText = 'Congratulations! You won! 😃';
-    popup.style.display = 'flex';
-  }
+	if (innerWord === selectedWord) {
+		finalMessage.innerText = 'Congratulations! You won! 😃';
+		popup.style.display = 'flex';
+	}
 }
 
 // Update the wrong letters
 function updateWrongLettersEl() {
-  // Display wrong letters
-  wrongLettersEl.innerHTML = `
+	// Display wrong letters
+	wrongLettersEl.innerHTML = `
     ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
     ${wrongLetters.map(letter => `<span>${letter}</span>`)}
   `;
 
-  // Display parts
-  figureParts.forEach((part, index) => {
-    const errors = wrongLetters.length;
+	// Display parts
+	figureParts.forEach((part, index) => {
+		const errors = wrongLetters.length;
 
-    if (index < errors) {
-      part.style.display = 'block';
-    } else {
-      part.style.display = 'none';
-    }
-  });
+		if (index < errors) {
+			part.style.display = 'block';
+		} else {
+			part.style.display = 'none';
+		}
+	});
 
-  // Check if lost
-  if (wrongLetters.length === figureParts.length) {
-    finalMessage.innerText = 'Unfortunately you lost. 😕';
-    popup.style.display = 'flex';
-  }
+	// Check if lost
+	if (wrongLetters.length === figureParts.length) {
+		finalMessage.innerText = 'Unfortunately you lost. 😕';
+		popup.style.display = 'flex';
+	}
 }
 
 // Show notification
 function showNotification() {
-  notification.classList.add('show');
+	notification.classList.add('show');
 
-  setTimeout(() => {
-    notification.classList.remove('show');
-  }, 2000);
+	setTimeout(() => {
+		notification.classList.remove('show');
+	}, 2000);
 }
 
 // Keydown letter press
 window.addEventListener('keydown', e => {
-  // console.log(e.keyCode);
-  if (e.keyCode >= 65 && e.keyCode <= 90) {
-    const letter = e.key;
+	// console.log(e.keyCode);
+	if (e.keyCode >= 65 && e.keyCode <= 90) {
+		const letter = e.key.toLowerCase();
 
-    if (selectedWord.includes(letter)) {
-      if (!correctLetters.includes(letter)) {
-        correctLetters.push(letter);
+		if (selectedWord.includes(letter)) {
+			if (!correctLetters.includes(letter)) {
+				correctLetters.push(letter);
 
-        displayWord();
-      } else {
-        showNotification();
-      }
-    } else {
-      if (!wrongLetters.includes(letter)) {
-        wrongLetters.push(letter);
+				displayWord();
+			} else {
+				showNotification();
+			}
+		} else {
+			if (!wrongLetters.includes(letter)) {
+				wrongLetters.push(letter);
 
-        updateWrongLettersEl();
-      } else {
-        showNotification();
-      }
-    }
-  }
+				updateWrongLettersEl();
+			} else {
+				showNotification();
+			}
+		}
+	}
 });
 
 // Restart game and play again
 playAgainBtn.addEventListener('click', () => {
-  //  Empty arrays
-  correctLetters.splice(0);
-  wrongLetters.splice(0);
+	//  Empty arrays
+	correctLetters.splice(0);
+	wrongLetters.splice(0);
 
-  selectedWord = words[Math.floor(Math.random() * words.length)];
+	selectedWord = words[Math.floor(Math.random() * words.length)];
 
-  displayWord();
+	displayWord();
 
-  updateWrongLettersEl();
+	updateWrongLettersEl();
 
-  popup.style.display = 'none';
+	popup.style.display = 'none';
 });
 
 displayWord();
