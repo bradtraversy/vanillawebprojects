005af58b4d7ac47f53f39b12eab1093ab2c1df98
tang0614 <tang0614@bu.edu>
2020-03-15 11:21:27 -0400

    first

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,4 +1,5 @@
-const form = document.getElementById('form');
+const form_1 = document.getElementById('form-1');
+const form_2 = document.getElementById('form-2');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
@@ -9,7 +10,7 @@ function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
   const small = formControl.querySelector('small');
-  small.innerText = message;
+  small.innerText = message; // show be textContent not innerText
 }
 
 // Show success outline
@@ -18,26 +19,11 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 
-// Check email is valid
-function checkEmail(input) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(input.value.trim())) {
-    showSuccess(input);
-  } else {
-    showError(input, 'Email is not valid');
-  }
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-// Check required fields
-function checkRequired(inputArr) {
-  inputArr.forEach(function(input) {
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
-    } else {
-      showSuccess(input);
-    }
-  });
-}
 
 // Check input length
 function checkLength(input, min, max) {
@@ -55,6 +41,16 @@ function checkLength(input, min, max) {
     showSuccess(input);
   }
 }
+// Check email is valid
+function checkEmail(input) {
+  //regular expression that matches an email
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
 
 // Check passwords match
 function checkPasswordsMatch(input1, input2) {
@@ -63,18 +59,30 @@ function checkPasswordsMatch(input1, input2) {
   }
 }
 
-// Get fieldname
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+// Check required fields
+function checkRequired(inputArr) {
+  //using arrow function
+  inputArr.forEach((input,index)=> {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
 }
 
 // Event listeners
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
+// An HTML event can be something the browser does, or  a user does.
+// and there will be function executed before the event occurs (such as when a user clicks a button).
+
+// An event occurs when a form is submitted
+form_1.addEventListener('submit', function(e) {
+  e.preventDefault(); // prevent form from submitting
 
   checkRequired([username, email, password, password2]);
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
   checkEmail(email);
   checkPasswordsMatch(password, password2);
+
 });
