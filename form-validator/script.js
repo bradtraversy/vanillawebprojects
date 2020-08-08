cac8e2012f9efb2a9e939e34f4eea75c21585337
Abraar Ahmed <60878757+abraarahmed@users.noreply.github.com>
2020-08-08 17:42:04 +0100

    Fix password2 length validation
    The last commit only compared password2 with password1, it didn't validate the length of password2; this fixes the issue of password2 length validation making it independent of password1.

--- form-validator/script.js
+++ form-validator/script.js
@@ -65,6 +65,7 @@ function checkPasswordsMatch(input1, input2) {
   if (input1.value !== input2.value) {
     showError(input2, 'Passwords do not match');
   }
+  checkLength(input2, 6, 25);
 }
 
 // Get fieldname
