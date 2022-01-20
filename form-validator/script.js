3791680bebd9a2505bc72b7e1fea35806e5a325a
MERT METİN <mertmetin1993@gmail.com>
2022-01-21 01:32:14 +0300

    Adding meaningful names for form inputs

--- form-validator/script.js
+++ form-validator/script.js
@@ -4,83 +4,96 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+
+//The list consists of meaningful name for inputs. Every meaningful names assigned element id name 
+//Ex.elementIdName: "Meaningful name
+const meaningfulNamesByElementId =
+{
+    username: "Username",
+    password2: "Confirm Password",
+    password: "Password",
+    email: "E-mail"
+}
+
 // Show input error message
 function showError(input, message) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control error';
-  const small = formControl.querySelector('small');
-  small.innerText = message;
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
 }
 
 // Show success outline
 function showSuccess(input) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control success';
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
 }
 
 // Check email is valid
 function checkEmail(input) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(input.value.trim())) {
-    showSuccess(input);
-  } else {
-    showError(input, 'Email is not valid');
-  }
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, `${getFieldName(input)} is not valid`);
+    }
 }
 
 // Check required fields
 function checkRequired(inputArr) {
-  let isRequired = false;
-  inputArr.forEach(function(input) {
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
-      isRequired = true;
-    } else {
-      showSuccess(input);
-    }
-  });
+    let isRequired = false;
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+            isRequired = true;
+        } else {
+            showSuccess(input);
+        }
+    });
 
-  return isRequired;
+    return isRequired;
 }
 
 // Check input length
 function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters`
-    );
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be less than ${max} characters`
-    );
-  } else {
-    showSuccess(input);
-  }
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} must be at least ${min} characters`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+        );
+    } else {
+        showSuccess(input);
+    }
 }
 
 // Check passwords match
 function checkPasswordsMatch(input1, input2) {
-  if (input1.value !== input2.value) {
-    showError(input2, 'Passwords do not match');
-  }
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
 }
 
 // Get fieldname
 function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    return (meaningfulNamesByElementId[input.id]) ?
+        meaningfulNamesByElementId[input.id] :
+        input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // Event listeners
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-  if(checkRequired([username, email, password, password2])){
-    checkLength(username, 3, 15);
-    checkLength(password, 6, 25);
-    checkEmail(email);
-    checkPasswordsMatch(password, password2);
-  }
+    if (checkRequired([username, email, password, password2])) {
+        checkLength(username, 3, 15);
+        checkLength(password, 6, 25);
+        checkEmail(email);
+        checkPasswordsMatch(password, password2);
+    }
 
 });
