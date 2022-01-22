904bc134e936c20d55bf43474333ac6ce2816994
MERT METİN <mertmetin1993@gmail.com>
2022-01-23 01:22:08 +0300

    Fixing "Next" button request url.

--- lyrics-search/script.js
+++ lyrics-search/script.js
@@ -7,93 +7,90 @@ const apiURL = 'https://api.lyrics.ovh';
 
 // Search by song or artist
 async function searchSongs(term) {
-  const res = await fetch(`${apiURL}/suggest/${term}`);
-  const data = await res.json();
+    const res = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await res.json();
 
-  showData(data);
+    showData(data);
 }
 
 // Show song and artist in DOM
 function showData(data) {
-  result.innerHTML = `
+    result.innerHTML = `
     <ul class="songs">
       ${data.data
-        .map(
-          song => `<li>
+            .map(
+                song => `<li>
       <span><strong>${song.artist.name}</strong> - ${song.title}</span>
       <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
     </li>`
-        )
-        .join('')}
+            )
+            .join('')}
     </ul>
   `;
 
-  if (data.prev || data.next) {
-    more.innerHTML = `
-      ${
-        data.prev
-          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
-          : ''
-      }
-      ${
-        data.next
-          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
-          : ''
-      }
+    if (data.prev || data.next) {
+        more.innerHTML = `
+      ${data.prev
+                ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+                : ''
+            }
+      ${data.next
+                ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+                : ''
+            }
     `;
-  } else {
-    more.innerHTML = '';
-  }
+    } else {
+        more.innerHTML = '';
+    }
 }
 
 // Get prev and next songs
-async function getMoreSongs(url) {
-  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
-  const data = await res.json();
-
-  showData(data);
+async function getMoreSongs(url) {   
+    const res = await fetch("https://cors-anywhere.herokuapp.com/" + url);
+    const data = await res.json();
+    showData(data);
 }
 
 // Get lyrics for song
 async function getLyrics(artist, songTitle) {
-  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
-  const data = await res.json();
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
 
-   if (data.error) {
+    if (data.error) {
         result.innerHTML = data.error;
-   } else {
+    } else {
         const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
 
         result.innerHTML = `
             <h2><strong>${artist}</strong> - ${songTitle}</h2>
             <span>${lyrics}</span>
         `;
-  }
+    }
 
-  more.innerHTML = '';
+    more.innerHTML = '';
 }
 
 // Event listeners
 form.addEventListener('submit', e => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const searchTerm = search.value.trim();
+    const searchTerm = search.value.trim();
 
-  if (!searchTerm) {
-    alert('Please type in a search term');
-  } else {
-    searchSongs(searchTerm);
-  }
+    if (!searchTerm) {
+        alert('Please type in a search term');
+    } else {
+        searchSongs(searchTerm);
+    }
 });
 
 // Get lyrics button click
 result.addEventListener('click', e => {
-  const clickedEl = e.target;
+    const clickedEl = e.target;
 
-  if (clickedEl.tagName === 'BUTTON') {
-    const artist = clickedEl.getAttribute('data-artist');
-    const songTitle = clickedEl.getAttribute('data-songtitle');
+    if (clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
 
-    getLyrics(artist, songTitle);
-  }
+        getLyrics(artist, songTitle);
+    }
 });
