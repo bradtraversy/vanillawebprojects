d654409a6a34a96236ca9d0bfbfa116de7977c9c
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2022-02-11 14:15:15 +0100

    Add hiding the nav on click outside of toggle or nav

--- modal-menu-slider/script.js
+++ modal-menu-slider/script.js
@@ -4,28 +4,20 @@ const open = document.getElementById('open');
 const modal = document.getElementById('modal');
 const navbar = document.getElementById('navbar');
 
-// This function closes navbar if user clicks anywhere outside of navbar once it's opened
-// Does not leave unused event listeners on
-// It's messy, but it works
-function closeNavbar(e) {
-  if (
-    document.body.classList.contains('show-nav') &&
-    e.target !== toggle &&
-    !toggle.contains(e.target) &&
-    e.target !== navbar &&
-    !navbar.contains(e.target)
-  ) {
-    document.body.classList.toggle('show-nav');
-    document.body.removeEventListener('click', closeNavbar);
-  } else if (!document.body.classList.contains('show-nav')) {
-    document.body.removeEventListener('click', closeNavbar);
-  }
-}
+// Hide nav on click outside of toggle or nav
+document.body.addEventListener('click', e => {
+  document.body.classList.contains('show-nav') &&
+  e.target != nav &&
+  !nav.contains(e.target) &&
+  e.target != toggle &&
+  !toggle.contains(e.target)
+    ? document.body.classList.toggle('show-nav')
+    : false;
+});
 
 // Toggle nav
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('show-nav');
-  document.body.addEventListener('click', closeNavbar);
 });
 
 // Show modal
