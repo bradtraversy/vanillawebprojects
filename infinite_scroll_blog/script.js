6ba2214e97e464ce08dc40b4a98669a16c9db7e7
SaadMu7ammad <74321306+SaadMu7ammad@users.noreply.github.com>
2023-04-26 18:02:07 +0200

    fix the order of the posts shown when scrolling

--- infinite_scroll_blog/script.js
+++ infinite_scroll_blog/script.js
@@ -1,80 +1,54 @@
-const postsContainer = document.getElementById('posts-container');
+const post = document.querySelector('.post');
+const postContainer = document.querySelector('.list');
+// const loading=document.querySelectorAll('.circle')
 const loading = document.querySelector('.loader');
-const filter = document.getElementById('filter');
+const filter = document.querySelector('input');
 
 let limit = 5;
 let page = 1;
-
-// Fetch posts from API
+///fetching
 async function getPosts() {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
   );
-
   const data = await res.json();
-
   return data;
 }
 
-// Show posts in DOM
+//show posts
 async function showPosts() {
-  const posts = await getPosts();
-
-  posts.forEach(post => {
-    const postEl = document.createElement('div');
-    postEl.classList.add('post');
-    postEl.innerHTML = `
-      <div class="number">${post.id}</div>
-      <div class="post-info">
-        <h2 class="post-title">${post.title}</h2>
-        <p class="post-body">${post.body}</p>
-      </div>
+  const p = await getPosts();
+  console.log(p);
+  p.forEach((post) => {
+    const newPost = document.createElement('div');
+    // newPost.classList.add('post');
+    newPost.className = 'post';
+    newPost.innerHTML = `<div class="number">${post.id}</div>
+    <div class="info">
+      <h3>${post.title}</h3>
+      <p>
+      ${post.body}
+      </p>
+    </div>
     `;
 
-    postsContainer.appendChild(postEl);
+    postContainer.appendChild(newPost);
   });
+  loading.classList.remove('show');
 }
 
-// Show loader & fetch more posts
 function showLoading() {
   loading.classList.add('show');
-
-  setTimeout(() => {
-    loading.classList.remove('show');
-
-    setTimeout(() => {
-      page++;
-      showPosts();
-    }, 300);
-  }, 1000);
+  page++;
+  showPosts();
 }
-
-// Filter posts by input
-function filterPosts(e) {
-  const term = e.target.value.toUpperCase();
-  const posts = document.querySelectorAll('.post');
-
-  posts.forEach(post => {
-    const title = post.querySelector('.post-title').innerText.toUpperCase();
-    const body = post.querySelector('.post-body').innerText.toUpperCase();
-
-    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
-      post.style.display = 'flex';
-    } else {
-      post.style.display = 'none';
-    }
-  });
-}
-
-// Show initial posts
-showPosts();
-
 window.addEventListener('scroll', () => {
+
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-  if (scrollHeight - scrollTop === clientHeight) {
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
     showLoading();
+    console.log('s');
   }
 });
-
-filter.addEventListener('input', filterPosts);
+showPosts();
